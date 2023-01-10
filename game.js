20de93c611edf19f219a5f8eeb448ebe2ab57f75
deco <medinaibanez03@outlook.com>
2023-01-09 20:22:59 -0700

    JS-question-additions

--- game.js
+++ game.js
@@ -1,48 +1,81 @@
-const question = document.getElementById("question");
-const choices = Array.from(document.getElementsByClassName("choice-text"));
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
 
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
-let availableQuestions = [];
+let availableQuesions = [];
 
 let questions = [
     {
-        question:"whos your daddy",
-        choice1:"your dad",
-        choice2:"your mom",
-        choice3:"me",
-        choice4:"you",
-        answer:3
+        question: "Commonly used data types DO NOT include",
+        choice1: "strings",
+        choice2: "booleans",
+        choice3: "alerts",
+        choice4: "numbers",
+        answer: 3,
     },
     {
-        question:"whos your daddy",
-        choice1:"your dad",
-        choice2:"your mom",
-        choice3:"me",
-        choice4:"you",
-        answer:3 
-    }
+        question:
+            "The condition in an if/else statement is enclosed with... ?",
+        choice1: "quotes",
+        choice2: "curly brackets",
+        choice3: "parenthesis",
+        choice4: "squarebrackets",
+        answer: 3,
+    },
     {
-        question:"whos your daddy",
-        choice1:"your dad",
-        choice2:"your mom",
-        choice3:"me",
-        choice4:"you",
-        answer:3
-    }
+        question: "Arrays in JavaScript can be used to store...?",
+        choice1: "numbers and strings",
+        choice2: "other Arrays",
+        choice3: "Booleans",
+        choice4: "All of the above",
+        answer: 4,
+    },
 ];
 
+//CONSTANTS
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3
+const MAX_QUESTIONS = 3;
 
 startGame = () => {
     questionCounter = 0;
     score = 0;
-    availableQuestions = [...questions];
-    console.log(availableQuestions);
-}
+    availableQuesions = [...questions];
+    getNewQuestion();
+};
 
+getNewQuestion = () => {
+    if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        //go to the end page
+        return window.location.assign('/end.html');
+    }
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+    currentQuestion = availableQuesions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach((choice) => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
+
+    availableQuesions.splice(questionIndex, 1);
+    acceptingAnswers = true;
+};
+
+choices.forEach((choice) => {
+    choice.addEventListener('click', (e) => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+        getNewQuestion();
+    });
+});
+
+startGame();
 
 
